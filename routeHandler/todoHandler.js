dc75a5e91aab4901525fc3e01bb4ebc1225b30b6
Joyanta Mondal <engr.joyantamondal@gmail.com>
2022-06-23 19:42:35 +0600

    first commit

---
+++ routeHandler/todoHandler.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const router = express.Router();
+const todoSchema = require('../schemas/todoSchema');
+
+const Todo = new mongoose.model("Todo",todoSchema);
+
+// Get All The Todos
+router.get('/',async(req,res)=>{
+
+})
+//get a todo by id
+router.get('/:id',async(req,res)=>{
+
+})
+//post a todo
+router.post('/',async(req,res)=>{
+    const newTodo = new Todo(req.body);
+    await newTodo.save((err)=>{
+        if(err){
+        res.status(500).json({
+        error:"There was a server side error!"
+        });
+        }else{
+        res.status(200).json({message:"Todo was inserted successfully..."});
+        }
+    });
+})
+
+//post multiple todo
+router.post('/all',async(req,res)=>{
+
+})
+
+//put  todo
+router.put('/:id',async(req,res)=>{
+
+})
+
+//put  todo
+router.put('/:id',async(req,res)=>{
+
+})
+
+// put  todo
+router.delete('/:id',async(req,res)=>{
+
+})
+module.exports= router;
